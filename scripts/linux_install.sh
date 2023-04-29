535afadf89cbbc961d059ca6fabf80b1fd61a047
David Sena <sena.ufc@gmail.com>
2023-04-29 16:16:39 -0300

    feat: adding linux_install script

---
+++ scripts/linux_install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function add_folder_to_path() {
+    local rc_file=$1
+    local folder_path=$2
+    local grep_result=$(grep -q -E "(^|:)$folder_path($|:)" "$rc_file" 2>/dev/null)
+
+    if [[ -z "$grep_result" ]]; then
+        echo "Adding $folder_path to $rc_file"
+        echo "export PATH=\$PATH:$folder_path" >> "$rc_file"
+    else
+        echo "Folder $folder_path already exists in $rc_file"
+    fi
+}
+
+if [[ -f ~/.bashrc ]]; then
+    add_folder_to_path ~/.bashrc ~/.tk
+fi
+
+if [[ -f ~/.zshrc ]]; then
+    add_folder_to_path ~/.zshrc ~/.tk
+fi
+
+
+# Set the URL and destination file path
+url="https://raw.githubusercontent.com/senapk/tk/master/tk.py"
+dest_path="$HOME/.tk/tk"
+
+# Create the destination directory if it doesn't exist
+mkdir -p "$HOME/.tk"
+
+# Download the file and save it to the destination path
+curl -o "$dest_path" "$url"
+
+# Make the file executable
+chmod +x "$dest_path"
