9f2798d0f4241690e775d3119e63ea417322365e
joaonorr <joaonoroes.jn@gmail.com>
2023-05-18 16:51:43 -0300

    fix: editando readme e modificando algumas modificações no setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name = 'testkit',
     version = '1.0.0',
     author = "David Sena Oliveira",
     author_email = "sena@ufc.br",
-    package = ['testkit'],
+    packages=find_packages(),
+    package_dir={'': '.'},
     description = "",
-    long_description = "",
+    long_description=long_description,
     url = "https://github.com/senapk/tk",
     project_urls = {
         'Código fonte': 'https://github.com/senapk/tk',
